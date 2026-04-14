21fd48ce6df2e0aafbc30efe93bc9c1b53048b6d
technoir <technoir.dev@gmail.com>
2026-04-14 18:07:10 +0100

    Use https scheme in license URL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -33,7 +33,7 @@ globalSettings {
     metadata {
         description = "Kotlin Multiplatform bindings for Vulkan API."
         developer(name = "technoir", email = "technoir.dev@gmail.com")
-        license(name = "The Apache Software License, Version 2.0", url = "http://www.apache.org/licenses/LICENSE-2.0.txt")
+        license(name = "The Apache Software License, Version 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.txt")
     }
 }
 
