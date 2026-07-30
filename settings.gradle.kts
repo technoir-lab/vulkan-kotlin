cc8666869e2ad7762a1913bdd426640bb37b476e
Sergey C. <technoir.dev@gmail.com>
2026-07-30 22:39:49 +0100

    Bump conventionPluginsVersion to v52 (#105)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         mavenLocal()
     }
     plugins {
-        val conventionPluginsVersion = "v50"
+        val conventionPluginsVersion = "v52"
         id("io.technoirlab.conventions.kotlin-multiplatform-application") version conventionPluginsVersion
         id("io.technoirlab.conventions.kotlin-multiplatform-library") version conventionPluginsVersion
         id("io.technoirlab.conventions.root") version conventionPluginsVersion
